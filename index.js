80f3ab9a8bde8e1be6cc86a3bb450a8373d87946
woldejamawolde <wolde.jama@m0ringaschool.com>
2023-10-04 23:30:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
+let num1 = 31;
+let num2 =2 ;
 // Write your code here
+ let multiply = num1 * num2;
+
+ const random = Math.floor(Math.random() * (Number.MAX_SAFE_INTEGER - 1)) + 1;
+
+
+let num3 = 11;
+let num4 = 7;
+let mod = num3 % num4;
+
+const numbers = [10, 5, 20, 15, 8];
+const max = Math.max(...numbers);
+
+console.log(max);
